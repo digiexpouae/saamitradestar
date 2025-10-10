cc1743179f8d87256e8aca1257f4665ac55d60b4
ramsha 1993 <rimshasaleem1993@gmail.com>
2025-10-10 20:54:42 +0500

    it support alignment fixed

--- src/components/ITsupport/section.jsx
+++ src/components/ITsupport/section.jsx
@@ -4,8 +4,8 @@ const section =()=>{
 
           <div className={`relative  text-center w-[90%] md:w-[80%] flex flex-col items-center gap-8 justify-between `}>
           <h2 className="text-[35px] md:text-[40px] w-full leading-[1]  text-black font-semibold ">IT Support</h2>   
-<p className="font-medium text-[20px] leading-[1.3] ">At Saami Logistics, we have invested in world-class technology to manage your supply chain. Our in-house system integrates transportation, inventory management, order fulfillment, financial settlement and e-commerce applications to give you the visibility and control you need.</p>
-<p className="font-medium text-[20px] leading-[1.3] ">It links you to our operations and other service providers to support inbound, outbound and reverse logistics. We simply connect your system with our applications using EDI capabilities.</p>
+<p className="font-medium text-[20px] leading-[1.3] text-[#5A5A5A]">At Saami Logistics, we have invested in world-class technology to manage your supply chain. Our in-house system integrates transportation, inventory management, order fulfillment, financial settlement and e-commerce applications to give you the visibility and control you need.</p>
+<p className="font-medium text-[20px] leading-[1.3] text-[#5A5A5A]">It links you to our operations and other service providers to support inbound, outbound and reverse logistics. We simply connect your system with our applications using EDI capabilities.</p>
 </div>
         </div>
     )
--- src/components/ITsupport/section2.jsx
+++ src/components/ITsupport/section2.jsx
@@ -1,11 +1,11 @@
 import Image from "next/image"
 const section2=({heading,text,classNamep})=>{
     return(
-        <div>
+        <div className="py-10">
             {/* Desktop */}
           <div className=" max-w-5xl mx-auto  p-6 md:p-10 m-auto  rounded-2xl gap-10 hidden md:flex flex-col md:flex-row  justify-between ">
               <div className=" w-1/2 text-center md:text-left"><h2 className="text-[30px] md:text-5xl  tracking-tighter font-semibold leading-[1.2] mb-4">{heading}</h2>
-              <p className={`font-medium text-[20px] leading-[1.3] ${classNamep}`}>{text}</p>
+              <p className={`font-medium text-[20px] leading-[1.3] text-[#5A5A5A] ${classNamep}`}>{text}</p>
           
               </div>
           <div className=" w-1/2"><Image src="/assets/ITSupport/it-1.png" alt="plane" width={400} height={900} className=" object-cover object-right" /></div>
@@ -15,7 +15,7 @@ const section2=({heading,text,classNamep})=>{
 <div className="h-auto  md:w-[40%]"><Image src="/assets/ITSupport/it-1.png" alt="plane" width={400} height={900} className="h-full w-full object-cover object-right " /></div>
 
               <div className="md:w-[55%] text-center md:text-left"><h2 className="text-[30px] md:text-[50px]  tracking-tighter font-semibold leading-[1.2] mb-4">{heading}</h2>
-              <p className={`font-medium text-[20px] leading-[1.3] ${classNamep}`}>{text}</p>
+              <p className={`font-medium text-[20px] leading-[1.3] text-[#5A5A5A] ${classNamep}`}>{text}</p>
           
               </div>
           </div> 
