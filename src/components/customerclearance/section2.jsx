5233cf43e9e53bec59352ecf305dba5f56a85deb
ramsha 1993 <rimshasaleem1993@gmail.com>
2025-10-10 20:49:14 +0500

    spare alignments fixed

--- src/components/customerclearance/section2.jsx
+++ src/components/customerclearance/section2.jsx
@@ -4,7 +4,7 @@ const section2=({heading})=>{
 return(
     <div className="flex flex-col   max-w-5xl px-4 mx-auto" >
         <h2 className="text-4xl font-medium  text-start md:pl-10  md:text-[40px] mb-2  leading-[1]  text-black font-semibold " >Custom Clearance Control</h2>
-    <div className=" hidden md:flex  p-10 pb-20 w-full  mx-auto">
+    <div className=" hidden md:flex gap-10  p-10 pb-20 w-full  mx-auto">
  <div className="w-1/2">
 
     <div className="font-medium  text-[20px] space-y-4 " style={{color:'rgba(90, 90, 90, 1)'}} >
@@ -22,12 +22,12 @@ return(
 
 </div>
 </div>
-<div>
-    <Image src={'/assets/spareparts/s-1.png'} alt="plane" width={400} height={600} className=" object-cover " />
+<div className="w-1/2">
+    <Image src={'/assets/spareparts/s-1.png'} alt="plane" width={500} height={600} className=" object-cover " />
 </div>
     </div>
     {/* mobile */}
-    <div className=" flex md:hidden gap-6  mb-10 max-w-5xl px-4 mx-auto items-center  flex-col justify-center mx-auto">
+    <div className=" flex md:hidden gap-6  mb-10 max-w-5xl px-4 mx-auto items-center py-10 flex-col justify-center mx-auto">
     <div>
     <Image src={'/assets/spareparts/s-1.png'} alt="plane" width={400} height={600} className=" object-cover  " />
 </div>
