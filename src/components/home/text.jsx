b1903ec8405da95ecd60b58afa412d0d75896927
ramsha1993 <rimshasaleem1993@gmail.com>
2026-02-25 15:26:12 +0500

    Fix:image fix

--- src/components/home/text.jsx
+++ src/components/home/text.jsx
@@ -54,7 +54,6 @@ const Text = ({ sectionRef }) => {
       animationRef.current = animation;
 
       const onReady = () => {
-        setIsLottieReady(true); // ← HERE (after SVG is injected)
 
         const totalFrames = animation.totalFrames;
         if (!totalFrames) return;
@@ -92,7 +91,7 @@ const Text = ({ sectionRef }) => {
                     scale: 1,
                     transformOrigin: "center center",
                   });
-                  gsap.to(imageref.current, { opacity: 1 - progress }, "<");
+                  gsap.to(videoRef.current, { opacity: 1 - progress }, "<");
                 } else if (progress < 0.4) {
                   setHideText(false);
                   gsap.to(sectionRef.current, {
@@ -136,7 +135,7 @@ const Text = ({ sectionRef }) => {
 
                     transformOrigin: "center center",
                   });
-                  gsap.to(imageref.current, { opacity: 1 - progress }, "<");
+                  gsap.to(videoRef.current, { opacity: 1 - progress }, "<");
                 }
                 else if (progress < 0.4) {
                   setHideText(false);
@@ -157,6 +156,8 @@ const Text = ({ sectionRef }) => {
             });
           },
         });
+        setIsLottieReady(true); // ← HERE (after SVG is injected)
+
       };
 
       animation.addEventListener("DOMLoaded", onReady);
@@ -182,21 +183,19 @@ const Text = ({ sectionRef }) => {
       {!hideText && <div className="absolute  top-1/2 lg:top-[70%] left-1/2 opacity-100 z-30 w-full -translate-x-1/2 -translate-y-1/2"> <h1 className="text-white text-center font-bold text-2xl md:text-4xl md:leading-[1.7]" >Reliable Logistics Solutions<br /> <span className="text-3xl md:text-5xl uppercase">Delivered with Precision.</span></h1>
         <p className="text-white text-lg text-center ">Seamless air, sea, and land freight services connecting your business to the world.</p> </div>}
 
-      <div className={`absolute inset-0 h-full w-full  transition-opacity duration-200 ease-in-out ${isLoaded ? 'opacity-100' : 'opacity-0'} `} ref={imageref}>
-        <video
-          ref={videoRef}
-          src="/assets/SaamiCompressed_two.mp4"
-          playsInline
-          muted
-          loop
-          // onCanPlayThrough={()=>handleVideoLoad()}
-          autoPlay
-          // FALLBACK for fast cache
-          onPlaying={() => setIsVideoReady(true)}
-          className={`h-full w-full object-cover  `}
+      <video
+        ref={videoRef}
+        src="/assets/SaamiCompressed_two.mp4"
+        playsInline
+        muted
+        loop
+        // onCanPlayThrough={()=>handleVideoLoad()}
+        autoPlay
+        // FALLBACK for fast cache
+        onPlaying={() => setIsVideoReady(true)}
+        className={` absolute inset-0 h-full w-full transition-opacity duration-200 ease-in-out object-cover ${isLoaded ? 'opacity-100' : 'opacity-0'} `}
 
-        ></video>
-      </div>
+      ></video>
 
       <div
         className={`absolute inset-0 transition-opacity duration-500 ease-in-out ${isLoaded && 'opacity-0'}`}
@@ -215,14 +214,13 @@ const Text = ({ sectionRef }) => {
         />
       </div >
 
-
-
+      {/* 
       <div
-        className="absolute inset-0  z-20 xl:h-[100vw] w-[100vw]"
+        className="absolute inset-0  z-20 xl:h-[100vw]   transition-opacity duration-500 ease-in  w-[100vw]"
         ref={container}
 
 
-      ></div >
+      ></div > */}
 
 
 
