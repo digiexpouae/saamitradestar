934a6eb95764bd6802defde0cb2359300f8ecde1
ramsha1993 <rimshasaleem1993@gmail.com>
2026-04-09 20:57:06 +0500

    Fix:Video updated

--- src/common/Video.jsx
+++ src/common/Video.jsx
@@ -1,12 +1,22 @@
-
+import { useEffect, useState } from "react"
 
 
 const parallexSection = ({ ref2 }) => {
+const [isMobile,setisMobile]=useState(false)
+
+
+useEffect(()=>{
+const checkMobile=()=> setisMobile(window.innerWidth<768);
+ checkMobile(); // run
+window.addEventListener("resize",checkMobile)
+return ()=> window.removeEventListener("resize",checkMobile)
+
+},[])
 
     return (
         <div className="w-full absolute h-full inset-0 z-[5] " ref={ref2}>
             <video
-                src="/assets/Saami2.mp4"
+                src={!isMobile ?"/assets/Final-video.mp4":"/assets/verticalVideo.mp4"}
                 playsInline
                 muted
                 loop
